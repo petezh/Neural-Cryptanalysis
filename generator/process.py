6a73a88683ad88af798fd13b31d84a9476ccafe4
petezh <thepeterszhang@gmail.com>
2019-06-23 03:21:44 -0400

    Frequency Analysis

--- generator/process.py
+++ generator/process.py
@@ -21,17 +21,27 @@ def process(fileName):
     for snip in snippets:
         
         shift = random.randint(1, 27)
-
         ciphertext = caesar(snip, shift)
-        
-        output.writerow([shift, ciphertext])
+        freq = frequency(ciphertext)
+        output.writerow([shift, freq])
 
-# perform encryption
+# perform a caesar cipher
 def caesar(snip, shift):
     alphabet = string.ascii_uppercase
     shifted_alphabet = alphabet[shift:] + alphabet[:shift]
     table = str.maketrans(alphabet, shifted_alphabet)
     return snip.translate(table)
-    
+
+# perform frequency analysis
+def frequency(text):
+
+    alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    freq = list()
+
+    for i in range(26):
+        freq.append(text.count(alpha[i]))
+
+    return freq
+
 if __name__ == '__main__':
     main()
